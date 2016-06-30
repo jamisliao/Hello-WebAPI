e5fcd900d9148db1a600f9f950f4f4907fd37431
Jamis Liao <Jamis Liao>
2016-06-30 15:57:03 +0800

    add project

---
+++ src/HelloWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWebAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        private ILogger _logger;
+
+        public HelloController(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<HelloController>();
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello WebAPI";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Student student)
+        {
+            this._logger.LogInformation($"Student ID : {student.Id}");
+            this._logger.LogInformation($"Student Name : {student.Name}");
+            this._logger.LogInformation($"Student Gender : {student.Gender}");
+        }
+    }
+}
---
+++ src/HelloWebAPI/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWebAPI.Model
+{
+    public class Student
+    {
+        /// <summary>
+        /// 學號
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 性別
+        /// </summary>
+        public string Gender { get; set; }
+    }
+}
